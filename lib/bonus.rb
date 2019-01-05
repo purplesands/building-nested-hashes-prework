7876454c67075a0b26b22b6eb036cd7739752c87
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 03:53:42 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -15,8 +15,8 @@ def bonus
    }
   }
   #code your solution here:
-    epic_tragedy[:montague][:hero][:status] = "dead"
-    epic_tragedy[:capulet][:hero][:status] = "dead"
+    epic_tragedy[:montague][:hero][:status] = "dead."
+    epic_tragedy[:capulet][:hero][:status] = "dead."
   epic_tragedy
 
 end
