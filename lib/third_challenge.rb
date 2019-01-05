cfb123e8a02cfd0f25235f30ecd8e13d790871ba
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 03:42:18 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,16 +10,16 @@ def third_challenge
       :hero => {
         :name => "Romeo",
         :age => "15",
-        :status => "alive"}
+        :status => "alive"},
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
         :name => "Lord Capulet",
-        :age => "50"}
+        :age => "50"},
       :matriarch => {
         :name => "Lady Capulet",
-        :age => "51"}
+        :age => "51"},
       :heroine => {
         :name => "Juliet",
         :age => "15",
